a7106112e349daacc90f873ed8d1bd2b736ac1cb
Shaun Bokenham <shaun@bokenham.net>
2018-06-21 00:20:47 +0100

    Add new calculations.

--- src/Controller/CalculatorController.php
+++ src/Controller/CalculatorController.php
@@ -5,12 +5,16 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\Calculations\AdditionCalculation;
+use App\Calculations\BitwiseAndCalculation;
+use App\Calculations\BitwiseOrCalculation;
+use App\Calculations\DivisionCalculation;
+use App\Calculations\MultiplicationCalculation;
 use App\Calculations\SubtractionCalculation;
 use App\Entity\CalculatorEntity;
 use App\Form\CalculatorType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CalculatorController extends Controller
@@ -25,6 +29,10 @@ class CalculatorController extends Controller
         $calculation = null;
         $calculatorEntity = (new CalculatorEntity())
             ->addCalculation(new AdditionCalculation())
+            ->addCalculation(new BitwiseAndCalculation())
+            ->addCalculation(new BitwiseOrCalculation())
+            ->addCalculation(new DivisionCalculation())
+            ->addCalculation(new MultiplicationCalculation())
             ->addCalculation(new SubtractionCalculation());
 
         $form = $this->createForm(CalculatorType::class, $calculatorEntity);
