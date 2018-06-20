21d5ff66ca74bb97759e9a67d64611c1c2a8cbe5
Shaun Bokenham <shaun@bokenham.net>
2018-06-21 00:20:18 +0100

    Add new calculations

---
+++ src/Calculations/MultiplicationCalculation.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Calculations;
+
+class MultiplicationCalculation implements CalculationInterface
+{
+    /**
+     * @return string
+     */
+    public function getName() : string
+    {
+        return 'Multiplication';
+    }
+
+    /**
+     * @param float $valueA
+     * @param float $valueB
+     *
+     * @return float
+     */
+    public function calculate(float $valueA, float $valueB) : float
+    {
+        return $valueA * $valueB;
+    }
+
+    /**
+     * @return string
+     */
+    public function __toString() : string
+    {
+        return $this->getName();
+    }
+}
