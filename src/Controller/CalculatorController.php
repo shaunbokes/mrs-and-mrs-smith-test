0b7206593250bc95a9f3107dd7c061ceff24e6dd
Shaun Bokenham <shaun@bokenham.net>
2018-06-21 00:10:36 +0100

    Handle form submission.

--- src/Controller/CalculatorController.php
+++ src/Controller/CalculatorController.php
@@ -4,16 +4,43 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Calculations\AdditionCalculation;
+use App\Calculations\SubtractionCalculation;
+use App\Entity\CalculatorEntity;
+use App\Form\CalculatorType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CalculatorController extends Controller
 {
-     /**
-      * @Route("/calculate")
-      */
-    public function calculateAction()
+    /**
+     * @Route("/calculate")
+     *
+     * @return Response
+     */
+    public function calculateAction(Request $request) : Response
     {
-        return $this->render('calculator/calculate.html.twig');
+        $calculation = null;
+        $calculatorEntity = (new CalculatorEntity())
+            ->addCalculation(new AdditionCalculation())
+            ->addCalculation(new SubtractionCalculation());
+
+        $form = $this->createForm(CalculatorType::class, $calculatorEntity);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $calculation = $calculatorEntity->getCalculation()->calculate(
+                $calculatorEntity->getValueA(),
+                $calculatorEntity->getValueB()
+            );
+        }
+
+        return $this->render('calculator/calculate.html.twig', [
+            'form' => $form->createView(),
+            'calculation' => $calculation
+        ]);
     }
 }
